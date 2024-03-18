9c56e31e096c6cfa0212e10a32b84b398811a796
GocasPT <gocasousa2013@gmail.com>
2024-03-18 19:34:44 +0000

    Base code for each project

---
+++ Cliente/main.cpp
@@ -0,0 +1,18 @@
+#include <io.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <fcntl.h> 
+#include <windows.h>
+
+int _tmain(int argc, TCHAR* argv[]) {
+	// VARIABLES
+
+#ifdef UNICODE
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+#endif 
+
+	_tprintf(TEXT("Sou o programa \'Cliente\'"));
+
+	return 0;
+}
