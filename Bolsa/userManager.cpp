ac73af75506480507f40b55ef288472e72ad77b5
GocasPT <gocasousa2013@gmail.com>
2024-04-29 18:50:35 +0100

    'userManager' doxygen comments :)

--- Bolsa/userManager.cpp
+++ Bolsa/userManager.cpp
@@ -2,7 +2,14 @@
 #include <algorithm>
 
 //TODO: check
-
+/**
+ * @brief Validar as credênciais do utilizador.
+ *
+ * @param userList - Lista de utilizadores.
+ * @param user - Utilizador a validar.
+ * 
+ * @return true se o utilizador for válido, false caso contrário.
+ */
 bool UserManager::validateUser(const USER_LIST& userList, USER user) {
 	auto it = std::find_if(userList.begin(), userList.end(),
 		[user](USER userItem) { return _tcscmp(userItem.name, user.name) == 0 && _tcscmp(userItem.password, user.password) == 0; });
@@ -13,9 +20,17 @@ bool UserManager::validateUser(const USER_LIST& userList, USER user) {
 }
 
 //TODO: check
+/**
+ * @brief Adiciona um utilizador ao servidor ou à fila de espera.
+ *
+ * @param servidor - Referencia do servidor.
+ * @param user - Ponteiro para o utilizador a adicionar.
+ * 
+ * @return true se o utilizador foi adicionado ao servidor, false se foi adicionado à fila de espera.
+ */
 bool UserManager::addUser(BOLSA& servidor, USER* user) {
 	if (servidor.hUsersList.size() < servidor.maxUsers) {
-		servidor.hUsersList.push_back(user->hReciverPipe);
+		servidor.hUsersList.push_back(user->hPipeInst);
 		user->connected = true;
 		return true;
 	} else {
@@ -26,16 +41,27 @@ bool UserManager::addUser(BOLSA& servidor, USER* user) {
 }
 
 //TODO: check
+/**
+ * @brief Remove um utilizador do servidor.
+ *
+ * @param userList - Lista de utilizadores.
+ * @param userQueue - Fila de utilizadores.
+ * @param user - Utilizador a remover.
+ * 
+ * @return Ponteiro para o utilizador que ficou em primeiro na fila de espera, ou nullptr se não houver utilizadores na fila.
+ * 
+ * @throw Se o utilizador não for encontrado.
+ */
 USER* UserManager::removeUser(USER_LIST& userList, USER_QUEUE& userQueue, USER* user) {
 	auto it = std::find_if(userList.begin(), userList.end(),
 				[user](USER userItem) { return _tcscmp(userItem.name, user->name) == 0; });
 
 	if (it == userList.end()) {
-		throw std::runtime_error("User not found"); //TODO: improve exception
+		throw std::runtime_error("Cliente não encontrado"); //TODO: improve exception
 	}
 
 	it->connected = false;
-	CloseHandle(it->hReciverPipe);
+	CloseHandle(it->hPipeInst);
 
 	if (userQueue.empty())
 		return nullptr;
@@ -48,6 +74,11 @@ USER* UserManager::removeUser(USER_LIST& userList, USER_QUEUE& userQueue, USER* 
 	return user;
 }
 
+/**
+ * @brief Lista todos os utilizadores do servidor.
+ *
+ * @param servidor - Referencia do servidor.
+ */
 void UserManager::listUsers(const BOLSA& servidor) {
 	std::_tcout << _T("Users:") << std::endl;
 
@@ -66,6 +97,16 @@ void UserManager::listUsers(const BOLSA& servidor) {
 }
 
 //TODO: check
+/**
+ * @brief Devolve a referência do utilizador com o nome especificado.
+ *
+ * @param userList - Lista de utilizadores.
+ * @param userName - Nome do utilizador a procurar.
+ * 
+ * @return Referência para o utilizador.
+ * 
+ * @throw Se o utilizador não for encontrado.
+ */
 USER& UserManager::getUser(USER_LIST& userList, std::TSTRING userName) {
 	auto it = std::find_if(userList.begin(), userList.end(),
 		[userName](USER user) { return userName.compare(user.name) == 0; });
@@ -78,6 +119,13 @@ USER& UserManager::getUser(USER_LIST& userList, std::TSTRING userName) {
 
 //TODO: check
 //TODO: we need this?
+/**
+ * @brief Liberta todos os utilizadores do servidor.
+ *
+ * Esta função deve ser chamada antes de terminar o servidor.
+ *
+ * @param servidor - Referencia do servidor.
+ */
 void UserManager::releaseUsers(BOLSA& servidor) {
 	USER_LIST& userList = servidor.userList;
 	USER_QUEUE& hUsersQueue = servidor.hUsersQueue;
